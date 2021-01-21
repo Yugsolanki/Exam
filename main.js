780584916d524f8b474c538e59e69aa29bf34d3c
Yug Solanki <yugsolanki15@gmail.com>
2021-01-21 15:08:52 +0530

    Full Website

---
+++ main.js
@@ -0,0 +1,36 @@
+var optionBox = document.querySelector('#option');
+
+function option() {
+  var options = ["A", "B", "C", "D"];
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function time() {
+  var sec = [30000, 45000, 60000, 90000, 120000]
+  return sec[Math.floor(Math.random() * sec.length)];
+    }
+  
+function sound() {
+  var sound = new Howl({
+    src: ['Assets/Music.webm', 'Assets/Music.mp3'],
+    autoautoplay: true,
+    loop: false,
+    volume: 0.5,
+    autoUnlock: true, 
+    onplayerror: function() {
+      sound.once('unlock', function() {
+        sound.play();
+      });
+    },
+  });
+  sound.play();
+  optionBox.textContent = option();
+}
+
+function play() {
+  
+var yug = time();
+  console.table(yug);
+  setTimeout(sound, yug);
+  optionBox.textContent = "";
+}
